5159f08c539c75500899225cadf4c2ebc4987e18
Rahulchallagundla <challagundlarahul@gmail.com>
2024-06-02 16:23:36 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,7 @@
+﻿// scripts.js
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you! You will be notified when we launch.');
+});
+
